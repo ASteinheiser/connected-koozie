567e08fdcd3ba39fe9f1ab0bf45fe9717662e7c3
ASteinheiser <asteinhe@asu.edu>
2017-02-28 11:23:15 -0700

    combined the values into one object that only updates when all values are collected

--- watcher.js
+++ watcher.js
@@ -4,6 +4,11 @@ var serviceUuids = ['19B10000E8F2537E4F6CD104768A1214'];
 
 var allowDuplicates = false;
 
+var Xdata = {'value': 0, 'changed': false};
+var Ydata = {'value': 0, 'changed': false};
+var Zdata = {'value': 0, 'changed': false};
+var accelData = {'X': 0, 'Y': 0, 'Z': 0};
+
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
     noble.startScanning(serviceUuids, allowDuplicates);
@@ -27,29 +32,56 @@ noble.on('discover', function(peripheral) {
         var characteristicZ = characteristics[2];
         console.log('discovered characteristics');
 
-        characteristicX.subscribe(function(error){
+        characteristicX.subscribe(function(error) {
           if(error) console.log(error);
         });
 
-        characteristicY.subscribe(function(error){
+        characteristicY.subscribe(function(error) {
           if(error) console.log(error);
         });
 
-        characteristicZ.subscribe(function(error){
+        characteristicZ.subscribe(function(error) {
           if(error) console.log(error);
         });
 
         characteristicX.on('data', function(data, isNotification) {
-          console.log('X: ', data.readUInt8(0));
+          if(Xdata.value !== data.readUInt8(0)) {
+            Xdata.value = data.readUInt8(0);
+            Xdata.changed = true;
+
+            updateAccelData();
+          }
         });
 
         characteristicY.on('data', function(data, isNotification) {
-          console.log('Y: ', data.readUInt8(0));
+          if(Ydata.value !== data.readUInt8(0)) {
+            Ydata.value = data.readUInt8(0);
+            Ydata.changed = true;
+
+            updateAccelData();
+          }
         });
 
         characteristicZ.on('data', function(data, isNotification) {
-          console.log('Z: ', data.readUInt8(0));
+          if(Zdata.value !== data.readUInt8(0)) {
+            Zdata.value = data.readUInt8(0);
+            Zdata.changed = true;
+
+            updateAccelData();
+          }
         });
+
+        function updateAccelData() {
+          if(Xdata.changed && Ydata.changed && Zdata.changed) {
+            accelData.X = Xdata.value;
+            accelData.Y = Ydata.value;
+            accelData.Z = Zdata.value;
+            Xdata.changed = false;
+            Ydata.changed = false;
+            Zdata.changed = false;
+            console.log(accelData);
+          }
+        }
       });
     });
   });
